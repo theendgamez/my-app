b90a89b02db3a27817d3c962264a2006b1df2249
theendgamez <theendgamez1@gmail.com>
2025-05-01 18:32:00 +0800

    rename2

--- src/components/eventCompo/Event.tsx
+++ src/components/eventCompo/Event.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import EventList from '@/components/event/Eventlist';
-import ListItem from '@/components/event/Listitem';
+import EventList from '@/components/eventCompo/EventList';
+import ListItem from '@/components/eventCompo/Listitem';
 import type { Events } from '@/types';
 
 interface Props {
